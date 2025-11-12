9f31167d1b272af57664ff84dbe45e86197401b3
Mohammed Maruf Islam <marufislam74208@gmail.com>
2025-11-12 16:57:53 +0600

    bug fux

--- lib/features/splash/controllers/splash_controller.dart
+++ lib/features/splash/controllers/splash_controller.dart
@@ -1,16 +1,26 @@
+import 'dart:async';
 import 'package:get/get.dart';
+
 import 'package:video_player/video_player.dart';
 
+import '../../auth/presentation/screens/login_screen.dart';
+
 class SplashController extends GetxController {
+  // final networkController = Get.find<NetworkController>();
   late final VideoPlayerController video;
   final RxBool isReady = false.obs;
+  final RxBool shouldShrink = false.obs;
 
-  static const double _ellipseTopIdle = 812.0;
-  static const double _ellipseTopPlaying = 666.0;
-  final RxDouble ellipseTop = _ellipseTopIdle.obs;
+  static const double _ellipseTopFinal = 666.0;
+  final RxDouble ellipseTop = _ellipseTopFinal.obs;
+
+  final RxBool showEllipse = false.obs;
   final RxBool showLogin = false.obs;
 
+  final Duration shrinkDelay = const Duration(milliseconds: 20);
   final Duration ellipseTriggerAt = const Duration(seconds: 2);
+  final Duration playDuration = const Duration(seconds: 3);
+
   bool _ellipseMoved = false;
 
   @override
@@ -25,35 +35,38 @@ class SplashController extends GetxController {
     await video.setVolume(0);
     await video.play();
     isReady.value = true;
+
+    Future.delayed(shrinkDelay, () {
+      shouldShrink.value = true;
+    });
+
     video.addListener(_progressWatcher);
-    video.addListener(_listenEnd);
+    video.addListener(_listenDuration);
   }
 
   void _progressWatcher() {
     final v = video.value;
     if (!_ellipseMoved && v.isInitialized && v.position >= ellipseTriggerAt) {
       _ellipseMoved = true;
-      startEllipseAnimation();
+      showEllipse.value = true;
       video.removeListener(_progressWatcher);
     }
   }
 
-  void startEllipseAnimation() {
-    ellipseTop.value = _ellipseTopPlaying;
-  }
-
-  void _listenEnd() {
+  void _listenDuration() async {
     final v = video.value;
-    if (v.isInitialized && v.position >= v.duration && !v.isPlaying) {
-      showLogin.value = true;
-      // Remove: Get.offAllNamed(AppRoute.getLoginScreen());
+    if (v.isInitialized && v.position >= playDuration) {
+      video.pause();
+      video.removeListener(_listenDuration);
+
+      Get.off(() => const LoginScreen());
     }
   }
 
   @override
   void onClose() {
     video.removeListener(_progressWatcher);
-    video.removeListener(_listenEnd);
+    video.removeListener(_listenDuration);
     video.dispose();
     super.onClose();
   }
--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:quikle_vendor/core/utils/constants/colors.dart';
+import 'package:quikle_vendor/features/auth/presentation/screens/login_screen.dart';
+import 'package:quikle_vendor/features/auth/presentation/screens/verification_scree.dart';
 import 'package:video_player/video_player.dart';
-import '../../../../core/utils/constants/colors.dart';
 import '../../controllers/splash_controller.dart';
-import '../widgets/box_skeleton.dart';
-import '../../../auth/presentation/screens/login_screen.dart';
 
 class SplashScreen extends GetView<SplashController> {
   const SplashScreen({super.key});
@@ -25,23 +25,39 @@ class SplashScreen extends GetView<SplashController> {
         statusBarIconBrightness: Brightness.dark,
       ),
       child: Scaffold(
-        backgroundColor: Colors.white,
         body: Stack(
           clipBehavior: Clip.none,
           children: [
-            Positioned(
-              left: 81.w,
-              top: 295.5.h,
-              child: Obx(() {
-                if (!controller.isReady.value) {
-                  return BoxSkeleton(width: 230.w, height: 221.h, radius: 48.r);
-                }
-                final vc = controller.video;
-                return ClipRRect(
-                  borderRadius: BorderRadius.circular(48.r),
-                  child: SizedBox(
-                    width: 230.w,
-                    height: 221.h,
+            Container(color: Colors.white),
+
+            /// Video animation
+            Obx(() {
+              final isReady = controller.isReady.value;
+              final shouldShrink = controller.shouldShrink.value;
+              final vc = controller.video;
+
+              if (!isReady) return const SizedBox.shrink();
+
+              return AnimatedPositioned(
+                duration: const Duration(milliseconds: 600),
+                curve: Curves.easeInOutCubic,
+                left: shouldShrink ? 81.w : 0,
+                top: shouldShrink ? 295.5.h : 0,
+                width: shouldShrink ? 250.w : 1.sw,
+                height: shouldShrink ? 240.h : 1.sh,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 600),
+                  curve: Curves.easeInOutCubic,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(
+                      shouldShrink ? 48.r : 0,
+                    ),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                      shouldShrink ? 48.r : 0,
+                    ),
                     child: FittedBox(
                       fit: BoxFit.cover,
                       child: SizedBox(
@@ -51,67 +67,86 @@ class SplashScreen extends GetView<SplashController> {
                       ),
                     ),
                   ),
-                );
-              }),
-            ),
+                ),
+              );
+            }),
+
+            /// Black ellipse animation
             Obx(() {
+              final showEllipse = controller.showEllipse.value;
+
               return AnimatedPositioned(
                 duration: const Duration(milliseconds: 450),
                 curve: Curves.easeInOut,
                 left: _ellipseLeft.w,
                 top: controller.ellipseTop.value.h,
-                child: ClipOval(
-                  child: SizedBox(
-                    width: 708.w,
-                    height: 331.h,
-                    child: Container(color: Colors.black),
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 250),
+                  curve: Curves.easeInOut,
+                  opacity: showEllipse ? 1 : 0,
+                  child: ClipOval(
+                    child: SizedBox(
+                      width: 708.w,
+                      height: 431.h,
+                      child: Container(color: Colors.black),
+                    ),
                   ),
                 ),
               );
             }),
+
+            /// Text on ellipse
             Obx(() {
               final double textTop =
                   (controller.ellipseTop.value + _textOffsetFromEllipseTop).h;
+              final showEllipse = controller.showEllipse.value;
               return AnimatedPositioned(
                 duration: const Duration(milliseconds: 450),
                 curve: Curves.easeInOut,
                 left: _textLeft.w,
                 top: textTop,
-                child: SizedBox(
-                  width: _textWidth.w,
-                  height: _textHeight.h,
-                  child: Center(
-                    child: RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: 'Fresh Food, ',
-                            style: TextStyle(
-                              fontFamily: 'Obviously',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 18.sp,
-                              height: 1.3,
-                              color: AppColors.eggshellWhite,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 250),
+                  curve: Curves.easeInOut,
+                  opacity: showEllipse ? 1 : 0,
+                  child: SizedBox(
+                    width: _textWidth.w,
+                    height: _textHeight.h,
+                    child: Center(
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'Things delivered ',
+                              style: TextStyle(
+                                fontFamily: 'Obviously',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18.sp,
+                                height: 1.3,
+                                color: AppColors.eggshellWhite,
+                              ),
                             ),
-                          ),
-                          TextSpan(
-                            text: 'Delivered Fast',
-                            style: TextStyle(
-                              fontFamily: 'Obviously',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 18.sp,
-                              height: 1.3,
-                              color: AppColors.beakYellow,
+                            TextSpan(
+                              text: 'Quickly',
+                              style: TextStyle(
+                                fontFamily: 'Obviously',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18.sp,
+                                height: 1.3,
+                                color: AppColors.beakYellow,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
               );
             }),
+
+            /// Login slide-up
             Obx(() {
               return AnimatedPositioned(
                 duration: const Duration(milliseconds: 600),
@@ -130,7 +165,6 @@ class SplashScreen extends GetView<SplashController> {
                             topRight: Radius.circular(24),
                           ),
                   ),
-
                   child: const LoginScreen(),
                 ),
               );
