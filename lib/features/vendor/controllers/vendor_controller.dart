4f50f14778b732e2435988ccb55fd9cbb4d0c9c4
NayanSoftDev <noyon040597@gmail.com>
2026-02-24 10:06:26 +0600

    update

--- lib/features/vendor/controllers/vendor_controller.dart
+++ lib/features/vendor/controllers/vendor_controller.dart
@@ -2,18 +2,14 @@ import 'package:get/get.dart';
 import '../models/vendor_model.dart';
 
 class VendorController extends GetxController {
-  // Observable for vendor details
   final vendorDetails = Rx<VendorDetailsModel?>(null);
 
-  /// Set vendor details from API response
   void setVendorDetails(Map<String, dynamic> vendorJson) {
     vendorDetails.value = VendorDetailsModel.fromJson(vendorJson);
   }
 
-  /// Get vendor details
   VendorDetailsModel? getVendorDetails() => vendorDetails.value;
 
-  /// Clear vendor details
   void clearVendorDetails() {
     vendorDetails.value = null;
   }
